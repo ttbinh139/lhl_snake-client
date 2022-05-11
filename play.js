2b2ec7bf5d7da634d22fc2290c8c7d64f6c2218e
ttbinh139 <ttbinh139@gmail.com>
2022-05-11 23:33:26 +0000

    Refactor play.js by moving function to handle user input to input.js

--- play.js
+++ play.js
@@ -3,30 +3,8 @@
   Port: 50541
  */
 
-const connect = require('./client');
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  const handleUserInput = function(key) {
-    if (key === '\u0003') {
-      process.exit();
-    } else {
-      console.log("Key", key);
-    }
-  };
-  stdin.on("data", handleUserInput);
-
-  
-  return stdin;
-};
-
-
-
+const {connect} = require('./client');
+const {setupInput} = require('./input');
 
 console.log("Connecting ...");
 connect();
