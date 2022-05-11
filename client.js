8a6afd123ea0233c9e0bf43df0b2a8d33acdfe6f
ttbinh139 <ttbinh139@gmail.com>
2022-05-11 23:18:44 +0000

    Send move command to server

--- client.js
+++ client.js
@@ -14,6 +14,28 @@ const connect = function () {
     conn.write("Name: ___");
   });
 
+  conn.on("connect", () => {
+    
+      for( i = 1; i < 20; i++) {
+        setTimeout(function(){
+          conn.write("Move: up");
+        }, i*500);
+
+        setTimeout(function(){
+          conn.write("Move: left");
+        }, (i + 6)*500);
+
+        setTimeout(function(){
+          conn.write("Move: down");
+        }, (i + 12)*500);
+
+        setTimeout(function(){
+          conn.write("Move: right");
+        }, (i + 18)*500);
+      }
+    
+  });
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
