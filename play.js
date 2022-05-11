58494a996761d6bd6c138983d53266fde9a057ee
ttbinh139 <ttbinh139@gmail.com>
2022-05-11 23:29:10 +0000

    Add function to handle user input

--- play.js
+++ play.js
@@ -3,17 +3,32 @@
   Port: 50541
  */
 
-
 const connect = require('./client');
 
-/* const config = {
-  host : `165.227.47.243`,
-  port : 50541
-}; */
+// setup interface to handle user input from stdin
 
-// establishes a connection with the game server
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    } else {
+      console.log("Key", key);
+    }
+  };
+  stdin.on("data", handleUserInput);
+
+  
+  return stdin;
+};
+
+
 
 
 console.log("Connecting ...");
 connect();
+setupInput();
 
